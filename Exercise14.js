565fd44cffd288a881f06906bb09cf9a1f217ad1
basyri <husain.basyri@gmail.com>
2019-01-13 15:09:36 +0700

    update again

--- Exercise14.js
+++ Exercise14.js
@@ -16,20 +16,22 @@ maka output: [ [ 20, 2 ], [ 7 ], [ 45, 21 ] ]
 function mengelompokkanAngka(arr) {
     var hasil = []
 
-    for (var i = 0; i < 3; i++) {
-        hasil.push([])
-    }
+    var evenNum = []
+    var oddNum = []
+    var varMod3 = []
+
 
     for (var j = 0; j < arr.length; j++) {
         if (arr[j] % 3 === 0) {
-            hasil[2].push(arr[j])
+            evenNum.push(arr[j])
         } else if (arr[j] % 2 !== 0) {
-            hasil[1].push(arr[j])
+            oddNum.push(arr[j])
         } else if (arr[j] % 2 === 0) {
-            hasil[0].push(arr[j])
+            varMod3.push(arr[j])
         }
-    }
 
+    }
+    hasil.push(varMod3, oddNum, evenNum)
     return hasil
 }
 
